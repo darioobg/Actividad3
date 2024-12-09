2c535df41189d6897e4bd01864d250aa4a2139ef
darioobg <dbriongosg02reducantabria.es>
2024-12-09 16:07:42 +0100

    He  actualizado el readme

---
+++ ConversorTemperaturas/src/main/java/ejemplo/conversortemperaturas/ConversorTemperaturas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejemplo.conversortemperaturas;
+
+/**
+ *
+ * @author DAW112
+ */
+public class ConversorTemperaturas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
